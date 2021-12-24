61b6fbbd801ce7bbd7c1271ab6a5e548db5caa2f
Cdreamfly <2290660986@qq.com>
2021-12-24 17:02:52 +0800

    添加了头文件和宏。

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+#include<sys/stat.h>
+#include<sys/errno.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<unistd.h>
+#include<ctype.h>
+
+#define SERVER_PORT 80  //端口
+#define SMALL_BUF 1024   //读取一行buffer大小
 
 int main(int argc,char**argv)
 {
